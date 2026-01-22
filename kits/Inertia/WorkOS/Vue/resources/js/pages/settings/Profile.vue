5549efd7d83a09948f98b08083903b103158f931
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-22 15:45:07 +0000

    Update Vue WorkOS Starter Kit files

--- kits/Inertia/WorkOS/Vue/resources/js/pages/settings/Profile.vue
+++ kits/Inertia/WorkOS/Vue/resources/js/pages/settings/Profile.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { Form, Head, usePage } from '@inertiajs/vue3';
-
 import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
 import DeleteUser from '@/components/DeleteUser.vue';
-import HeadingSmall from '@/components/HeadingSmall.vue';
+import Heading from '@/components/Heading.vue';
 import InputError from '@/components/InputError.vue';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -12,10 +10,11 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
 import { edit } from '@/routes/profile';
 import { type BreadcrumbItem } from '@/types';
+import { Form, Head, usePage } from '@inertiajs/vue3';
 
-interface Props {
+type Props = {
     status?: string;
-}
+};
 
 defineProps<Props>();
 
@@ -38,7 +37,8 @@ const user = page.props.auth.user;
 
         <SettingsLayout>
             <div class="flex flex-col space-y-6">
-                <HeadingSmall
+                <Heading
+                    variant="small"
                     title="Profile information"
                     description="Update your name and email address"
                 />
