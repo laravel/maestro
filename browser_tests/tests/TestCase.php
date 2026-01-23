76e3a5663c004d92a4578b48db15261c9be8203d
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-23 12:47:23 +0000

    Remove not needed configuration for browser tests

--- browser_tests/tests/TestCase.php
+++ browser_tests/tests/TestCase.php
@@ -3,9 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use PHPUnit\Framework\Attributes\RequiresEnvironmentVariable;
 
-#[RequiresEnvironmentVariable('APP_BASE_PATH')]
 abstract class TestCase extends BaseTestCase
 {
     //
