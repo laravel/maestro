6e33d3cd772e196ea5295c0eae9768c1d159d373
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-27 12:20:33 +0000

    Create personal team when creating a new user

---
+++ kits/Shared/Teams/Fortify/app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Concerns\PasswordValidationRules;
+use App\Concerns\ProfileValidationRules;
+use App\Enums\TeamRole;
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules, ProfileValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        Validator::make($input, [
+            ...$this->profileRules(),
+            'password' => $this->passwordRules(),
+        ])->validate();
+
+        return DB::transaction(function () use ($input) {
+            $user = User::create([
+                'name' => $input['name'],
+                'email' => $input['email'],
+                'password' => $input['password'],
+            ]);
+
+            $this->createPersonalTeam($user);
+
+            return $user;
+        });
+    }
+
+    /**
+     * Create a personal team for the user.
+     */
+    protected function createPersonalTeam(User $user): void
+    {
+        $team = Team::create([
+            'name' => $user->name."'s Team",
+            'is_personal' => true,
+        ]);
+
+        $team->members()->attach($user, [
+            'model_type' => $user::class,
+            'role' => TeamRole::Owner->value,
+            'is_default' => true,
+        ]);
+    }
+}
