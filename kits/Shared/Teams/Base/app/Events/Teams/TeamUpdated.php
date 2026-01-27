8b108fcae040fb98507b0619a81c05042dfd23a1
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-27 14:02:09 +0000

    Add events, notifications, policies and middleware for team feature

---
+++ kits/Shared/Teams/Base/app/Events/Teams/TeamUpdated.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Events\Teams;
+
+use App\Models\Team;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TeamUpdated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public Team $team
+    ) {}
+}
