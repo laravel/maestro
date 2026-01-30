8c7be8309e245823b994a4091dfd5dec2321a5db
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-30 12:17:26 +0000

    Fix issues with dashboard route on WorkOS variant

---
+++ kits/Shared/Teams/WorkOS/routes/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\WorkOS\Http\Requests\AuthKitAuthenticationRequest;
+use Laravel\WorkOS\Http\Requests\AuthKitLoginRequest;
+use Laravel\WorkOS\Http\Requests\AuthKitLogoutRequest;
+
+Route::get('login', function (AuthKitLoginRequest $request) {
+    return $request->redirect();
+})->middleware(['guest'])->name('login');
+
+Route::get('authenticate', function (AuthKitAuthenticationRequest $request) {
+    $request->authenticate();
+
+    $user = auth()->user();
+    $currentTeam = $user->currentTeam ?? $user->personalTeam();
+
+    if ($currentTeam && ! $user->current_team_id) {
+        $user->update(['current_team_id' => $currentTeam->id]);
+    }
+
+    return redirect()->intended(route('dashboard', ['current_team' => $currentTeam?->slug]));
+})->middleware(['guest']);
+
+Route::post('logout', function (AuthKitLogoutRequest $request) {
+    return $request->logout();
+})->middleware(['auth'])->name('logout');
