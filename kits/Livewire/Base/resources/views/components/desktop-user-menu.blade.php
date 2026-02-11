97b0cc4f61077c46ab816bad1b2a266c2cdf1119
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-02-11 13:50:02 +0000

    Fix issue with special characters on desktop sidebar menu (#19)

--- kits/Livewire/Base/resources/views/components/desktop-user-menu.blade.php
+++ kits/Livewire/Base/resources/views/components/desktop-user-menu.blade.php
@@ -1,6 +1,6 @@
 <flux:dropdown position="bottom" align="start">
     <flux:sidebar.profile
-        {{ $attributes->only('name') }}
+        :name="auth()->user()->name"
         :initials="auth()->user()->initials()"
         icon:trailing="chevrons-up-down"
         data-test="sidebar-menu-button"
