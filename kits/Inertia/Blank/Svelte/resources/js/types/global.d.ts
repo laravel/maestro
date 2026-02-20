3cfff979764871ac763e39b6663ac1f9cd6adb3b
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-02-20 15:05:32 -0800

    wip

--- kits/Inertia/Blank/Svelte/resources/js/types/global.d.ts
+++ kits/Inertia/Blank/Svelte/resources/js/types/global.d.ts
@@ -1,7 +1,8 @@
-import type { Auth } from '@/types/auth';
+import type { Auth } from "@/types/auth";
+import type { Team } from "./teams";
 
 // Extend ImportMeta interface for Vite...
-declare module 'vite/client' {
+declare module "vite/client" {
     interface ImportMetaEnv {
         readonly VITE_APP_NAME: string;
         [key: string]: string | boolean | undefined;
@@ -16,12 +17,14 @@ declare module 'vite/client' {
     }
 }
 
-declare module '@inertiajs/core' {
+declare module "@inertiajs/core" {
     export interface InertiaConfig {
         sharedPageProps: {
             name: string;
             auth: Auth;
             sidebarOpen: boolean;
+            currentTeam: Team | null;
+            teams: Team[];
             [key: string]: unknown;
         };
     }
