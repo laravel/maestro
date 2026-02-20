1de117762259d392df9b291dea334bb6336b9492
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-02-20 15:05:32 -0800

    wip

--- kits/Inertia/Blank/React/resources/js/types/global.d.ts
+++ kits/Inertia/Blank/React/resources/js/types/global.d.ts
@@ -1,11 +1,14 @@
-import type { Auth } from '@/types/auth';
+import type { Auth } from "@/types/auth";
+import type { Team } from "./teams";
 
-declare module '@inertiajs/core' {
+declare module "@inertiajs/core" {
     export interface InertiaConfig {
         sharedPageProps: {
             name: string;
             auth: Auth;
             sidebarOpen: boolean;
+            currentTeam: Team | null;
+            teams: Team[];
             [key: string]: unknown;
         };
     }
