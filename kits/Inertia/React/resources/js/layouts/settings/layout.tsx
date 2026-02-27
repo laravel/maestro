a75149dbe60a690f3ff4b9b3d78a9e46c9ce5bfd
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-02-27 10:34:50 +0000

    Improve how to check for URL on Inertia starter kits and how to route to dashboard on Livewire starter kit

--- kits/Inertia/React/resources/js/layouts/settings/layout.tsx
+++ kits/Inertia/React/resources/js/layouts/settings/layout.tsx
@@ -35,7 +35,7 @@ const sidebarNavItems: NavItem[] = [
 ];
 
 export default function SettingsLayout({ children }: PropsWithChildren) {
-    const { isCurrentUrl } = useCurrentUrl();
+    const { isCurrentOrParentUrl } = useCurrentUrl();
 
     // When server-side rendering, we only render the layout on the client...
     if (typeof window === 'undefined') {
@@ -62,7 +62,7 @@ export default function SettingsLayout({ children }: PropsWithChildren) {
                                 variant="ghost"
                                 asChild
                                 className={cn('w-full justify-start', {
-                                    'bg-muted': isCurrentUrl(item.href),
+                                    'bg-muted': isCurrentOrParentUrl(item.href),
                                 })}
                             >
                                 <Link href={item.href}>
