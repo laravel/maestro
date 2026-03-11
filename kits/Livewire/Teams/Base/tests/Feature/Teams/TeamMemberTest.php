d9d42880a1f8242c603a2f06d2159a82265eff51
Taylor Otwell <taylor@laravel.com>
2026-03-11 13:57:51 -0500

    test formatting

--- kits/Livewire/Teams/Base/tests/Feature/Teams/TeamMemberTest.php
+++ kits/Livewire/Teams/Base/tests/Feature/Teams/TeamMemberTest.php
@@ -18,6 +18,7 @@ class TeamMemberTest extends TestCase
         $owner = User::factory()->create();
         $member = User::factory()->create();
         $team = Team::factory()->create();
+
         $team->members()->attach($owner, ['role' => TeamRole::Owner->value]);
         $team->members()->attach($member, ['role' => TeamRole::Member->value]);
 
@@ -39,6 +40,7 @@ class TeamMemberTest extends TestCase
         $admin = User::factory()->create();
         $member = User::factory()->create();
         $team = Team::factory()->create();
+
         $team->members()->attach($owner, ['role' => TeamRole::Owner->value]);
         $team->members()->attach($admin, ['role' => TeamRole::Admin->value]);
         $team->members()->attach($member, ['role' => TeamRole::Member->value]);
@@ -55,6 +57,7 @@ class TeamMemberTest extends TestCase
         $owner = User::factory()->create();
         $member = User::factory()->create();
         $team = Team::factory()->create();
+
         $team->members()->attach($owner, ['role' => TeamRole::Owner->value]);
         $team->members()->attach($member, ['role' => TeamRole::Member->value]);
 
@@ -68,12 +71,13 @@ class TeamMemberTest extends TestCase
         $this->assertFalse($member->fresh()->belongsToTeam($team));
     }
 
-    public function test_team_member_cannot_be_removed_by_non_owner(): void
+    public function test_team_member_cannot_be_removed_by_non_owners(): void
     {
         $owner = User::factory()->create();
         $admin = User::factory()->create();
         $member = User::factory()->create();
         $team = Team::factory()->create();
+
         $team->members()->attach($owner, ['role' => TeamRole::Owner->value]);
         $team->members()->attach($admin, ['role' => TeamRole::Admin->value]);
         $team->members()->attach($member, ['role' => TeamRole::Member->value]);
@@ -86,14 +90,16 @@ class TeamMemberTest extends TestCase
             ->assertForbidden();
     }
 
-    public function test_removed_member_current_team_is_set_to_personal_team(): void
+    public function test_removed_members_current_team_is_set_to_personal_team(): void
     {
         $owner = User::factory()->create();
         $member = User::factory()->create();
         $personalTeam = $member->personalTeam();
         $team = Team::factory()->create();
+
         $team->members()->attach($owner, ['role' => TeamRole::Owner->value]);
         $team->members()->attach($member, ['role' => TeamRole::Member->value]);
+
         $member->update(['current_team_id' => $team->id]);
 
         $this->actingAs($owner);
