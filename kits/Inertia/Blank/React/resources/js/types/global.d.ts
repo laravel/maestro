556d994849810d8c504ece13f95002af683d1b78
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-05-13 13:48:15 -0400

    Update global.d.ts

--- kits/Inertia/Blank/React/resources/js/types/global.d.ts
+++ kits/Inertia/Blank/React/resources/js/types/global.d.ts
@@ -1,5 +1,11 @@
 import type { Auth } from '@/types/auth';
 
+declare module 'react' {
+    interface InputHTMLAttributes<T> {
+        passwordrules?: string;
+    }
+}
+
 declare module '@inertiajs/core' {
     export interface InertiaConfig {
         sharedPageProps: {
