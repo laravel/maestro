f28ae9dd5b543b81c1c83163976470d27ebdb85f
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-05-27 12:22:21 +0100

    Update with latest changes from Laravel Skeleton

--- kits/Livewire/Teams/Base/bootstrap/app.php
+++ kits/Livewire/Teams/Base/bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\SetTeamUrlDefaults;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(
+            fn (Request $request) => $request->is('api/*'),
+        );
     })->create();
