65e699111ece7ce4ff3d4b3105b4b214b6a76be3
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-01 15:14:13 +0100

    Remove manual API Teams invitation response docs

--- kits/API/Teams/app/Http/Controllers/Teams/DeclineTeamInvitationController.php
+++ kits/API/Teams/app/Http/Controllers/Teams/DeclineTeamInvitationController.php
@@ -8,14 +8,11 @@ use App\Http\Responses\MessageResponse;
 use App\Models\TeamInvitation;
 use Dedoc\Scramble\Attributes\Endpoint;
 use Dedoc\Scramble\Attributes\Group;
-use Dedoc\Scramble\Attributes\Response as ScrambleResponse;
-use Illuminate\Http\Response;
 
 #[Group('Team Invitations')]
 class DeclineTeamInvitationController extends Controller
 {
     #[Endpoint(title: 'Decline an invitation', description: 'Decline a pending team invitation for the authenticated user.')]
-    #[ScrambleResponse(Response::HTTP_OK, 'Invitation declined.', type: 'array{message: string}')]
     public function __invoke(AcceptTeamInvitationRequest $request, TeamInvitation $invitation): MessageResponse
     {
         $invitation->delete();
