eec0c402fe9fa0e9089d6d8ac4f34546ee7532cf
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-03 11:18:36 +0100

    Clean up orchestrator dependencies and routes

--- orchestrator/bootstrap/app.php
+++ orchestrator/bootstrap/app.php
@@ -2,15 +2,8 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        commands: __DIR__.'/../routes/console.php',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
